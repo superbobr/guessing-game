d5d3ec623de6d6279b249f232d0d13f6981a5fbf
Natein <natein@gmail.com>
2025-04-24 21:13:26 +0300

    feat: Standardize task format (#54)

---
+++ src/index.js
@@ -0,0 +1,16 @@
+class GuessingGame {
+  constructor() {
+    throw new Error('Not implemented');
+  }
+  /*
+  setRange(min, max) { }
+
+  guess() { }
+
+  lower() { }
+
+  greater() { }
+*/
+}
+
+module.exports = GuessingGame;
